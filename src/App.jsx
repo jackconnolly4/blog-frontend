9b5f2af2e84f02e89dba8b1e5331c678d7d0470c
Jack <jconnolly444@gmail.com>
2023-11-03 13:47:28 -0500

    reformmated data for parent/children exercise

--- src/App.jsx
+++ src/App.jsx
@@ -22,37 +22,18 @@ function PostsNew() {
   );
 }
 
-function PostsIndex() {
+function PostsIndex(props) {
+  console.log(props);
   return (
     <div id="posts-index">
       <h1>All posts</h1>
-      <div>
-        <h2>Blog Post 1</h2>
-        <div>
-          <img src="https://1000logos.net/wp-content/uploads/2016/11/google_logo.png" alt="googl logo" />
+      {props.posts.map((post) => (
+        <div key={post.id}>
+          <h2>{post.title}</h2>
+          <img src={post.image} alt="logos" />
+          <p>{post.body}</p>
         </div>
-        <p>This is the first post</p>
-      </div>
-      <div>
-        <h2>Blog Post 2</h2>
-        <div>
-          <img
-            src="https://static.wikia.nocookie.net/ween/images/d/d1/Kisspng-ween-tabernacle-concert-boognish-logo-wolves-and-moose-on-isle-royale-5b3bad7b1d6e06.4733215815306376911206.png/revision/latest?cb=20190410165641"
-            alt="boognish"
-          />
-        </div>
-        <p>This is the second post</p>
-      </div>
-      <div>
-        <h2>Blog Post 3</h2>
-        <div>
-          <img
-            src="radihttps://static.wikia.nocookie.net/logopedia/images/6/6f/Radioheadmodifiedbear.svg/revision/latest?cb=20210529183052ohead.com"
-            alt="radiohead logo"
-          />
-        </div>
-        <p>This is the third post</p>
-      </div>
+      ))}
     </div>
   );
 }
@@ -66,10 +47,32 @@ function Footer() {
 }
 
 function Content() {
+  let posts = [
+    {
+      id: 1,
+      title: "Blog Post 1",
+      body: "This is the first post",
+      image: "https://1000logos.net/wp-content/uploads/2016/11/google_logo.png",
+    },
+    {
+      id: 2,
+      title: "Blog Post 2",
+      body: "This is the second post",
+      image:
+        "https://static.wikia.nocookie.net/ween/images/d/d1/Kisspng-ween-tabernacle-concert-boognish-logo-wolves-and-moose-on-isle-royale-5b3bad7b1d6e06.4733215815306376911206.png/revision/latest?cb=20190410165641",
+    },
+    {
+      id: 3,
+      title: "Blog Post 3",
+      body: "This is the third post",
+      image:
+        "https://static.wikia.nocookie.net/logopedia/images/6/6f/Radioheadmodifiedbear.svg/revision/latest?cb=20210529183052ohead.com",
+    },
+  ];
   return (
     <div>
       <PostsNew />
-      <PostsIndex />
+      <PostsIndex posts={posts} />
     </div>
   );
 }
