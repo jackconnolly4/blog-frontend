e83122083a5d3fe82fb8f5f66b7dc74761e9351f
Jack <jconnolly444@gmail.com>
2023-11-10 13:38:19 -0600

    added patch/update ability to PostsShow page

--- src/PostsShow.jsx
+++ src/PostsShow.jsx
@@ -1,11 +1,31 @@
+import axios from "axios";
+
 export function PostsShow(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    axios.patch(`http://localhost:3000/posts/${props.post.id}.json`, params).then((response) => {
+      console.log(response);
+      event.target.reset();
+    });
+  };
   console.log(props);
   return (
     <div>
-      <h2>Title: {props.post.title}</h2>
-      <p>body: {props.post.body}</p>
-      <img src={props.post.image} />
-      <p>Created at: {props.post.created_at}</p>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <div>
+            Title: <input defaultValue={props.post.title} name="title" type="text" />
+          </div>
+          <div>
+            Body: <input defaultValue={props.post.body} name="body" type="text" />
+          </div>
+          <div>
+            Image URL: <input defaultValue={props.post.image} name="image_url" type="text" />
+          </div>
+          <button type="submit">Update Post</button>
+        </div>
+      </form>
     </div>
   );
 }
