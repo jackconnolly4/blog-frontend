8db8f15cd073aec33747a045abd048337bb7b489
Jack <jconnolly444@gmail.com>
2023-12-04 14:00:56 -0600

    added search function to PostsIndex page

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 export function PostsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
   console.log(props);
   return (
     <div id="posts-index">
+      <form className="d-flex" role="search">
+        Search filter:{" "}
+        <input type="text" value={searchFilter} onChange={(event) => setSearchFilter(event.target.value)} />
+        <button className="btn btn-outline-success" type="submit">
+          Search
+        </button>
+      </form>
       <h1>All posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id}>
-          <h2>{post.title}</h2>
-          <img src={post.image} alt="logos" />
-          <p>{post.body}</p>
-          <button onClick={() => props.onShowPost(post)}>More info!</button>
-        </div>
-      ))}
+      {props.posts
+        .filter((post) => post.body.toLowerCase().includes(searchFilter.toLowerCase()))
+        .map((post) => (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+            <img src={post.image} alt="logos" />
+            <p>{post.body}</p>
+            <button onClick={() => props.onShowPost(post)}>More info!</button>
+          </div>
+        ))}
     </div>
   );
 }
