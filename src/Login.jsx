dcfcf5be6b4739f9d4cb58af1ea1365289fdf583
Jack <jconnolly444@gmail.com>
2023-12-04 11:48:18 -0600

    added character limit for SignUp username

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 
 const jwt = localStorage.getItem("jwt");
+
 if (jwt) {
   axios.defaults.headers.common["Authorization"] = `Bearer ${jwt}`;
 }
